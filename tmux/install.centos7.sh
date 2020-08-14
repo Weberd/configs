4648a8c2ab4728c8c8161b1d72e59cb16b0d11be
Weberd <weberdster@gmail.com>
2020-08-14 17:14:31 +0400

    install tpm

--- tmux/install.centos7.sh
+++ tmux/install.centos7.sh
@@ -1,3 +1,7 @@
 #/bin/sh
 
 yum install ius-release && yum install tmux2u
+
+# install tmux plugin manager
+git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+tmux source ~/.tmux.conf
