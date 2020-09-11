7e0004b9ca9482d6e06fb673142d137576b9fe6e
Weberd <weberdster@gmail.com>
2020-09-11 17:36:09 +0400

    Update install.centos7.sh

--- tmux/install.centos7.sh
+++ tmux/install.centos7.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
-yum install ius-release && yum install tmux2u
+sudo yum install https://repo.ius.io/ius-release-el$(rpm -E '%{rhel}').rpm
+sudo yum install tmux2u
 
 # install tmux plugin manager
 git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
