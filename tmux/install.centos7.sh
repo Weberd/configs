4d3005f933726937991ed3e12516312e5044a8cc
Weberd <weberdster@gmail.com>
2020-10-05 10:25:48 +0400

    Update install.centos7.sh

--- tmux/install.centos7.sh
+++ tmux/install.centos7.sh
@@ -6,3 +6,5 @@ sudo yum install tmux2u
 # install tmux plugin manager
 git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
 tmux source ~/.tmux.conf
+
+# In tmux hit prefix + I to fetch the plugin and source it. You should now be able to use the plugin.
