c7f31e782bee5bfdac36c5e3d75ba1b9d3fbf009
Weberd <weberdster@gmail.com>
2021-01-02 14:46:18 +0400

    Update rdp.sh

--- i3wm/rdp.sh
+++ i3wm/rdp.sh
@@ -4,5 +4,4 @@
 # 1 - leave on server (or laptop)
 # 2 - disable audio
 
-xfreerdp /v:192.168.1.100 /u:alex-di /p:123 /sound /audio-mode:1 /bpp:15 /f +toggle-fullscreen
-
+xfreerdp /v:192.168.1.100 /u:alex-di /p:123 /sound /audio-mode:1 /f +toggle-fullscreen -wallpaper +fonts
