156a5a5164001ff39c578b21a7165b911fa5c054
Weberd <weberdster@gmail.com>
2021-01-02 14:46:51 +0400

    Update rdp.sh

--- i3wm/rdp.sh
+++ i3wm/rdp.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
-# 0 - redirect
-# 1 - leave on server (or laptop)
+# audio:
+# 0 - redirect to client
+# 1 - leave on server
 # 2 - disable audio
 
 xfreerdp /v:192.168.1.100 /u:alex-di /p:123 /sound /audio-mode:1 /f +toggle-fullscreen -wallpaper +fonts
