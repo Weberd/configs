23b9327762457d0e008c28605aec3dcddcb2d2cd
Weberd <weberdster@gmail.com>
2021-07-26 18:48:59 +0400

    Delete rdp.sh

--- i3wm/rdp.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# audio:
-# 0 - redirect to client
-# 1 - leave on server
-# 2 - disable audio
-
-xfreerdp /cert-ignore /v:192.168.1.100 /u:alex-di /p:123 /sound /audio-mode:1 /f /gfx-h264:AVC420 +toggle-fullscreen -wallpaper +fonts
