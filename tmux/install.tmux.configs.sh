aae71252d57b2c930a015c76ef1bd17431951c23
Weberd <weberdster@gmail.com>
2022-12-27 17:22:32 +0500

    Update and rename install.tmux.configs to install.tmux.configs.sh

--- tmux/install.tmux.configs.sh
+++ tmux/install.tmux.configs.sh
@@ -1,3 +1,5 @@
+#!/bin/sh
+
 # install tmux plugin manager
 git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
 wget https://raw.githubusercontent.com/Weberd/configs/master/tmux/.tmux.conf -O ~/.tmux.conf
