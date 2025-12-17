0095d6af425ddde70a5a3613a11093fac988805e
Weberd <weberdster@gmail.com>
2025-12-17 11:30:54 +0000

    Delete rdp.sh

--- rdp.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# audio:
-# 0 - redirect to client
-# 1 - leave on server
-# 2 - disable audio
-
-xfreerdp /cert-ignore /v:192.168.1.100 /u:username /p:password /sound /audio-mode:0 /f +toggle-fullscreen -wallpaper +fonts
