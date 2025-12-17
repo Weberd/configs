a81290e7e089df0467505fa878fa65f29ef7b3cd
Weberd <weberdster@gmail.com>
2025-12-17 10:52:02 +0000

    Delete tmux/install.tmux.configs.sh

--- tmux/install.tmux.configs.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-# install tmux plugin manager
-git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
-wget https://raw.githubusercontent.com/Weberd/configs/master/tmux/.tmux.conf -O ~/.tmux.conf
-tmux source ~/.tmux.conf
-
-echo in tmux press Shift+I to reload environment
